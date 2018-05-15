2e14ccf486ff1e3575f6fca785831d985a0cef37
student <leowqh>
2018-05-15 12:50:43 +0800

    Added forms

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ server.get('/', (req,res) => {
   res.render('main.hbs');
 });
 
+server.get('/main', (req,res) => {
+  res.render('main.hbs');
+});
+
+server.get('/result', (req,res) => {
+  res.render('result.hbs');
+});
+
+server.post('/form', (req,res) => {
+  res.render('form.hbs');
+});
+
 server.post('/getweather', (req, res) => {
   const addr = req.body.address;
 
@@ -25,13 +37,14 @@ server.post('/getweather', (req, res) => {
     const weatherReq = `https://api.darksky.net/forecast/ffa4a25b863a01ce16341c70fa8ee28b/${lat},${lng}`;
     return axios.get(weatherReq);
   }).then((response) => {
-    res.send(
-      {
-        address: addr,
-        summary: response.data.currently.summary,
-        temperature: (response.data.currently.temperature - 32) * 0.5556,
-      }
-    );
+
+
+
+    res.render('result.hbs', {
+      address: addr,
+      summary: response.data.currently.summary,
+      temperature: (response.data.currently.temperature - 32) * 0.5556,
+    });
 
     console.log(response.data.currently.summary);
     const temp = (response.data.currently.temperature - 32) * 0.5556;
